9e825a182cb15908f19a0654b2b4d7b33b50bfa1
zwa47 <zwa47@asb9838s-d06.csil.sfu.ca>
2013-09-24 19:46:17 -0700

    dependency parser interface

--- parse.py
+++ parse.py
@@ -10,9 +10,9 @@
 from nltk.parse.nonprojectivedependencyparser import NonprojectiveDependencyParser
 from nltk import parse_dependency_grammar
 
-def init(dg):
+def dep_init(dg):
     """
-    init(DependencyGrammar) -> NonprojectiveDependencyParser
+    dep_init(DependencyGrammar) -> NonprojectiveDependencyParser
 
     To initialize the non-projective dependency with dependency grammar. The
     grammar can be pre-trained data, or, can be generated using the function
@@ -26,9 +26,9 @@ def init(dg):
     p = NonprojectiveDependencyParser(dg)
     return p
 
-def parse_sentence(parser,sent):
+def dep_parse_sentence(parser,sent):
     """
-    parse_sentence(NonprojectiveDependencyParser,str) -> list
+    dep_parse_sentence(NonprojectiveDependencyParser,str) -> list
 
     This function accepts a string sentence and a parser object as input,
     and returns the result of parsing as output. The sentence will be transformed
@@ -41,15 +41,26 @@ def parse_sentence(parser,sent):
     """
     return parser.parse(sent.split())
 
-def parse_list(parser,sent):
+def dep_parse_list(parser,sent):
     """
-    parse_list(NonprojectiveDependencyParser,list) -> list
+    dep_parse_list(NonprojectiveDependencyParser,list) -> list
 
-    This is almost the same as parse_sentence, except that the 2nd argumemt
+    This is almost the same as dep_parse_sentence, except that the 2nd argumemt
     is a list contaning each token of a sentence, instead of a string. The
     return value is also the same.
     """
     return parser.parse(sent)
+
+def check_dep_return_value(ret):
+    """
+    check_dep_return_value(list/bool) -> None
+
+    Check the retuen value in case that a False is encountered.
+    """
+    if ret == False:
+        raise ValueError('The return value of the dependency parse is False.')
+    else:
+        return
 
 def get_dep_tree(ds,index):
     """
@@ -61,8 +72,16 @@ def get_dep_tree(ds,index):
 
     This function will return a tree (i.e. a list) rooted at ds[index]. The
     whole tree can be got by calling with index = 0.
+
+    The structure of the tree is expliained below: Each node is repersented
+    as a list, the first element is the word, i.e. the token; the second element
+    is some additional information, which can be determined by the user, and
+    this function will not add anything to this item, just leave a None there;
+    the third element is also a list, which contains all the leaves of this node.
+    This structure is recursively defined, which is exactly what a tree looks like.
     """
-    d = ds[index]
+    # Just the same as indexing
+    d = ds.get_by_address(index)
     if d.has_key('tag') and d['tag'] == 'TOP':
         sub_trees = [d['deps']]
     else:
@@ -77,17 +96,81 @@ def get_dep_tree(ds,index):
             returned_sub_trees.append(ret)
         return [d['word'],None,returned_sub_trees]
 
+def get_all_dep_trees(trees):
+    """
+    get_all_dep_trees(list) -> list
+
+    Given the result of a non-projective dependency parser, which is a list,
+    this function will return a list of trees.
+    """
+    check_dep_return_value(trees)
+    new_trees = []
+    for i in trees:
+        new_trees.append(get_dep_tree(i,0))
+        
+    return new_trees
+
+#########################################
+# Grammar Training ######################
+#########################################
+
+def insert_dep_entry(grm,head,modes):
+    """
+    insert_dep_entry(list,str,list) -> None
+
+    This function will insert a rule into the grammar. The rule is made of a head
+    and a mode (or modes). The head is actually the left hand side and the mode
+    is the right hand side. modes is a list containing all modes of a particular
+    head. This function will change the grammar in-place.
+
+    For example:
+
+    insert_dep_entry(grammar,'shot',['I','elephant','in'])
+
+    will insert a grammar rule 'shot' -> 'I' | 'elephant' | 'in' into the list.
+    """
+    grm.append((head,modes,))
+    return
+
+def dep_add_qutation_mark(s):
+    return "'" + s + "'"
+
+def convert_dep_into_str(grm):
+    """
+    convert_dep_into_str(grm) -> str
+
+    Return the string representation of the dependency grammar. Used for training
+    using parse_dependency_grammar()
+    """
+    s = ""
+    for i in grm:
+        lhs = dep_add_qutation_mark(i[0])
+        rhs = dep_add_qutation_mark(i[1][0])
+        for j in i[1][1:]:
+            rhs += ' | ' + dep_add_qutation_mark(j)
+        s += ('\n' + lhs + ' -> ' + rhs)
+    return s
+    
 dg = parse_dependency_grammar("""
 'shot' -> 'I' | 'elephant' | 'in'
 'elephant' -> 'an' | 'in'
 'in' -> 'pajamas'
 'pajamas' -> 'my'
 """)
-psr = init(dg)
+psr = dep_init(dg)
+help(dg)
+
+def debug_convert_dep_into_str():
+    grm = []
+    insert_dep_entry(grm,'shot',['I','elephant','in'])
+    insert_dep_entry(grm,'in',['pajamas'])
+    insert_dep_entry(grm,'elephant',['an','in'])
+    insert_dep_entry(grm,'pajamas',['my'])
+    print parse_dependency_grammar(convert_dep_into_str(grm))
 
 def debug_get_dep_tree():
-    trees = parse_sentence(psr,'I shot an elephant in my pajamas')
-    print get_dep_tree(trees[0],0)
+    trees = dep_parse_sentence(psr,'I shot an elephant in my pajamas')
+    print get_all_dep_trees(trees)
 
 if __name__ == '__main__':
-    debug_get_dep_tree()
+    debug_convert_dep_into_str()
