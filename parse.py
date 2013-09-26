f30c3bf752660ad353eb7dba3437dd3b879db544
zwa47 <zwa47@asb9838s-d09.csil.sfu.ca>
2013-09-26 11:22:06 -0700

    pickle used

--- parse.py
+++ parse.py
@@ -1,4 +1,6 @@
 from nltk.parse.nonprojectivedependencyparser import *
+import pickle
+
 #help(ProbabilisticNonprojectiveParser)
 
 def print_lable_dist(lable_dist,fp):
@@ -28,8 +30,41 @@ def print_train_data(lable_dist,feature_dist,filename="TAGParseData.dat"):
     print_feature_dist(feature_dist,fp)
     fp.close()
 
+def dump_to_disk(filename,obj):
+    """
+    Dump an object into the disk using pickle
+    :param filename: The name of the dumping file
+    :type filename: str
+    :param obj: Any object you want to dump
+    :type obj: Any object
+    """
+    fp = open(filename,'wb')
+    pickle.dump(obj,fp)
+    fp.close()
+
+def restore_from_disk(filename):
+    """
+    Restore the dumped file using pickle to an obejct
+    :param filename: The file you want to read from
+    :type filename: str
+    :return: The restored object
+    :rtype: Any object
+    """
+    fp = open(filename,'rb')
+    obj = pickle.load(fp)
+    fp.close()
+    return obj
+
 def debug_print_train_data():
     print_train_data(None,None)
 
+def debug_dump_and_restore():
+    d = {'a': 1,'b': 2,'c':3,None:4}
+    e = ['ed','ser','56','wertwert']
+    f = {3:d,5:e,7:'dffdfdf'}
+    dump_to_disk('dict.dat',f)
+    rd = restore_from_disk('dict.dat')
+    print rd
+
 if __name__ == '__main__':
-    debug_print_train_data()
+    debug_dump_and_restore()
