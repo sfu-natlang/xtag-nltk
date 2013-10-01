b104a1863940e6858e77c9e105bceabbee8cfbcb
wangziqi2013 <675973885@qq.com>
2013-10-01 02:00:58 -0700

    Commit before sleep, nothing changed

--- parse.py
+++ parse.py
@@ -1,3 +1,12 @@
+# Natural Language Toolkit: Tree-Adjoining Grammar
+#
+# Copyright (C) 2001-2013 NLTK Project
+# Author: WANG Ziqi, Haotian Zhang <{zwa47,haotianz}@sfu.ca>
+#
+# URL: <http://www.nltk.org/>
+# For license information, see LICENSE.TXT
+#
+
 from nltk.parse.nonprojectivedependencyparser import *
 import pickle
 from copy import deepcopy
