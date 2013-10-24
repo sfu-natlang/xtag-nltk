b00ecfa9b236dbb45b47ab253b953770298cd1bc
zwa47 <zwa47@asb9838s-c05.csil.sfu.ca>
2013-10-24 10:44:13 -0700

    regexp fixed

--- load.py
+++ load.py
@@ -983,65 +983,6 @@ def analyze_tree_5(xtag_trees):
 #    return new_feature
             
 
-# This function acceps a normalized feature structure (which uses __value__
-# as the last level of indexing) and a regular expression, and returns
-# a new feature structure that match the regexp or do not match.
-def match_feature(feature,regexp,operation=0):
-    """
-    match_feature(feature,regexp,operation=0) -> FeatStruct
-
-    This function is used to filter a feature structure with a regular exression.
-    The regular expression should be written in the form that XTAG system uses,
-    which has a '__value__' entry at the last level of indexing.
-
-    feature: The feature that you would like to filter
-
-    regexp: An acceptable regular expression by module re
-
-    operation: 0 if positive filtering will be done, 1 if negative filtering.
-    positive filtering means that all RHS values that match the regexp will be
-    retained, while negative filtering means that all RHS values that doesn't
-    match will be retained.
-    """
-    new_feature = FeatStruct()
-    count = 0
-    for i in feature.keys():
-        val = feature[i]
-        if test_leaf(val) == True:
-            search_ret = re.search(regexp,i)
-            if operation == 0 and search_ret != None:
-                new_feature[i] = val
-                count += 1
-            elif operation == 1 and search_ret == None:
-                new_feature[i] = val
-                count += 1
-        else:
-            search_ret = re.search(regexp,i)
-            if operation == 0 and search_ret != None:
-                new_feature[i] = val
-                count += 1
-            elif operation == 1 and search_ret == None:
-                ret = match_feature(val,regexp,operation)
-                if ret != None:
-                    new_feature[i] = ret
-                else:
-                    new_feature[i] = FeatStruct()
-                count += 1
-            elif operation == 1 and search_ret != None:
-                pass
-            else:
-                ret = match_feature(val,regexp,operation)
-                #print ret,'\n'
-                if ret != None:
-                    new_feature[i] = ret
-                    count += 1
-
-    #print new_feature,'\n'
-    if count == 0:
-        return None
-    else:
-        return new_feature
-
 ######################################
 # Word and feature conversion ########
 ######################################
