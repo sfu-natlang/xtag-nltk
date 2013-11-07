e461d50961b5b1680308d3766d467a5cf0f5cc2e
hawtyzhang <hawtysfu@gmail.com>
2013-11-06 20:12:16 -0800

    Update __int__.py

--- __int__.py
+++ __int__.py
@@ -1,1 +1,8 @@
-
+# Natural Language Toolkit: Tree-Adjoining Grammar
+#
+# Copyright (C) 2001-2013 NLTK Project
+# Author: WANG Ziqi, Haotian Zhang <{zwa47,haotianz}@sfu.ca>
+#
+# URL: <http://www.nltk.org/>
+# For license information, see LICENSE.TXT
+#
