c7939cf7f167d5095b2eb54ac3806adb6b8a1eca
Anoop Sarkar <anoop.sarkar@gmail.com>
2014-04-17 17:39:44 -0700

    cleaned up installation and added howto to README

--- README.md
+++ README.md
@@ -1,17 +1,26 @@
 NLTK interface to the XTAG grammar files in multiple languages.
 
+Authors:
+
 * Ziqi Wang
 * Haotian Zhang
 * Anoop Sarkar
 
-SFU Natural Language Lab: http://natlang.cs.sfu.ca/
+[SFU Natural Language Lab](http://natlang.cs.sfu.ca/)
 
-Functionalities
+For now the code implements a viewer for the XTAG grammar format.
+It has been tested with the XTAG English grammar and the XTAG Korean
+grammar.
 
-<ul>
-<li>XTAG Grammar Viewer</li>
-<li>XTAG Parser</li>
-<li>XTAG grammar generator</li>
-</ul>
+Future plans include a parser and grammar generator.
 
-Original XTAG interface and project web page: http://www.cis.upenn.edu/~xtag/
+Original XTAG interface and [XTAG project web page](http://www.cis.upenn.edu/~xtag/)
+
+Installation
+------------
+
+0. Remove any previous data in `~/nltk_data/xtag_grammar` if previously installed.
+1. Run `sh install.sh`. This will install the english and korean grammars into `~/nltk_data/xtag_grammar`.
+2. Run `python draw.py english` to view the XTAG English grammar.
+3. Run `python draw.py korean` to view the XTAG Korean grammar.
+
---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+download_cmd="wget "
+
+if [ ! -d ~/nltk_data/xtag_grammar ]; then
+    mkdir -p ~/nltk_data/xtag_grammar
+fi
+cd ~/nltk_data/xtag_grammar
+if ! type "wget" > /dev/null; then
+ 	if ! type "curl" > /dev/null; then
+    	echo "Need wget or curl installed."
+    	exit
+	fi
+fi
+if type "curl" > /dev/null; then
+	download_cmd="curl -LOk "
+fi
+if [ ! -d korean ]; then
+	cmd=$download_cmd$"https://github.com/sfu-natlang/xtag-korean-grammar/archive/master.zip"
+	eval $cmd
+	unzip master 
+	mv xtag-korean-grammar-master korean
+	rm -f master.zip 
+	
+fi
+if [  ! -d english ]; then
+	cmd=$download_cmd$"https://github.com/sfu-natlang/xtag-english-grammar/archive/master.zip"
+	eval $cmd
+	unzip master 
+	mv xtag-english-grammar-master english
+	rm -f master.zip
+fi
+
+eval $download_cmd$"https://raw.github.com/sfu-natlang/xtag-nltk/master/util.py"
+eval $download_cmd$"https://raw.github.com/sfu-natlang/xtag-nltk/master/load.py"
+eval $download_cmd$"https://raw.github.com/sfu-natlang/xtag-nltk/master/feature.py"
+eval $download_cmd$"https://raw.github.com/sfu-natlang/xtag-nltk/master/LL1.py"
+
+python util.py english
+python util.py korean
+rm -f *.{py,pyc}
